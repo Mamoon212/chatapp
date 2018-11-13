46157f576d8a9fb05f34875ec94d765802d8da32
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-13 11:04:53 +0000

    add styling

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@
             
             jQuery("#message-form").on("submit", function(e){
                 e.preventDefault();
+                var messageTextBox= jQuery('[name=message]');
                 socket.emit("createMessage", {
                     from: "user",
-                    text: jQuery('[name=message]').val()
+                    text: messageTextBox.val()
                 }, function(){
-                    
+                    messageTextBox.val('')
                 })
             })
             
@@ -41,12 +42,15 @@
                 if(!navigator.geolocation){
                     return alert("Geolocation not supported by this browser")
                 }
+                locationButton.attr("disabled", "disabled").text("Sending location...");
                 navigator.geolocation.getCurrentPosition(function(position){
+                    locationButton.removeAttr("disabled").text("Send location")
                     socket.emit("createLocationMessage",{
                         latitude:position.coords.latitude,
                         longitude: position.coords.longitude
                     })
                 }, function(){
+                    locationButton.removeAttr("disabled").text("Send location")
                     alert("Unable to fetch location")
                 })
             })
