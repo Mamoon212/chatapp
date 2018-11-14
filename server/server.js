0189f314c14f072741de1cd42257b45e258d4b23
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-14 16:10:15 +0000

    start to add private rooms

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express= require("express");
 const socketIO= require("socket.io");
 const http= require("http");
 const {generateMessage, generateLocationMessage}= require("./utils/message");
+const {isRealString}= require("./utils/validation");
 var app= express();
 var server= http.createServer(app);
 var io= socketIO(server);
@@ -13,9 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log("new user connected");
     
-    socket.emit("newMessage",generateMessage("Admin","Welcome to the chat app"))
-    socket.broadcast.emit("newMessage",generateMessage("Admin", "A new user has joined"))
+   
+    socket.on("join",(params, callback)=>{
+        if(!isRealString(params.name) || !isRealString(params.room)){
+            callback("Name and room name are required");
+        } else{
+            socket.join(params.room);
+            socket.emit("newMessage",generateMessage("Admin","Welcome to the chat app"))
+            socket.broadcast.to(params.room).emit("newMessage",generateMessage("Admin", `${params.name} has joined`))
     
+            callback();
+        }
+    })
         socket.on("createMessage",(message, callback)=>{
         console.log("createMessage", message);
         io.emit("newMessage", generateMessage(message.from, message.text))
