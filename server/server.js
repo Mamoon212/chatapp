e9a48045b59963376bb4dd30e74fa79f515449f4
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-16 22:31:24 +0000

    fix and add user list

--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ io.on('connection', (socket)=>{
           users.removeUser(socket.id);
           users.addUser(socket.id, params.name, params.room);
           io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-            users.addUser(socket.id, params.name, params.room);
             socket.emit("newMessage",generateMessage("Admin","Welcome to the chat app"))
             socket.broadcast.to(params.room).emit("newMessage",generateMessage("Admin", `${params.name} has joined`))
     
