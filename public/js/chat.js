b6c0134eed64396a4e2a9f2b36d59db8076a5901
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-16 23:35:04 +0000

    add name and private rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -70,7 +70,6 @@
                 e.preventDefault();
                 var messageTextBox= jQuery('[name=message]');
                 socket.emit("createMessage", {
-                    from: "user",
                     text: messageTextBox.val()
                 }, function(){
                     messageTextBox.val('')
